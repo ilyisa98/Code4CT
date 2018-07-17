01584491b286d269a9862034e43e1611dbd9c49d
Ilyaaz Isaacs <38458373+ilyisa98@users.noreply.github.com>
2018-07-17 13:11:32 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,44 +2,44 @@
 
 //slider
 
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function currentDiv(n) {
-  showDivs(slideIndex = n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  
-  if (n > x.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = x.length} ;
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
-  }
-  x[slideIndex-1].style.display = "block";  
-
-}
+//var slideIndex = 1;
+//showDivs(slideIndex);
+//
+//function currentDiv(n) {
+//  showDivs(slideIndex = n);
+//}
+//
+//function showDivs(n) {
+//  var i;
+//  var x = document.getElementsByClassName("mySlides");
+//  
+//  if (n > x.length) {slideIndex = 1}    
+//  if (n < 1) {slideIndex = x.length} ;
+//  for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";  
+//  }
+//  x[slideIndex-1].style.display = "block";  
+//
+//}
 
 
 //automatic slider
 
-//var myIndex = 0;
-//carousel();
-//
-//function carousel() {
-//    var i;
-//    var x = document.getElementsByClassName("mySlides");
-//    for (i = 0; i < x.length; i++) {
-//       x[i].style.display = "none";  
-//    }
-//    
-//    myIndex++;
-//    if (myIndex > x.length) {myIndex = 1}    
-//    x[myIndex-1].style.display = "block";  
-//    setTimeout(carousel, 5000); // Change image every 2 seconds
-//}
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 5000); // Change image every 2 seconds
+}
 
 
 //count up
--- js/main_courses.js
+++ js/main_courses.js
@@ -1,90 +1,202 @@
- var items = document.getElementsByClassName('item');
-// console.log(items);
+'use strict;'
 
-//var cart = [
-//     { name: "beginnerJs", price: 1800,},
-//     { name: "beginnerFsw", price: 1800,},
-//     { name: "beginnerPyth", price: 1800,},
-//     { name: "intermediateJs", price: 1800,},
-//     { name: "intermediateFsw", price: 1200,},
-//     { name: "intermediatePyth", price: 1800,},
-//];
+     var products = [];
+    var cart = [];
 
-var cart = [
-    // { name: name, price: price,},
-    // { name: name, price: price,},
-    // { name: name, price: price,},
-    // { name: name, price: price,},
-    // { name: name, price: price,},
-    // { name: name, price: price,},
-];
+        function addProduct(productID, qty, duration, price) {
+  
+            var newProduct = {
+                product_id: null,
+                product_qty: 0,
+                product_dura: 0,
+                product_price: 0.00,
+            };
+            
+            newProduct.product_id = productID;
+            newProduct.product_qty = qty;
+            newProduct.product_dura = duration;
+            newProduct.product_price = price;
 
-function addCart (name, price) {
+                //check if array empty
+            if(products.length != 0){
+                //search an item in an array
+                var check = false;
+                for(let productsItem in products){
+                    if(products[productsItem].product_id === newProduct.product_id){
+                        check = true;
+                    }  
+                }
 
-    //create item
-    let item = {
-        name: name, 
-        price: price,
-    };
-    
-//     console.log(item);
+                //if check returns false,  item was not found in the array.
+                if(!check){ products.push(newProduct)}
 
-    //check if array empty
-    if(cart.length != 0){
-        //search an item in an array
-        var check = false;
-        for(let cartItem in cart){
-            if(cart[cartItem].name === item.name){
-                check = true;
-            }  
+                //else push to array
+            } else {
+                products.push(newProduct);
+            }
+            
+            
+            console.log(products);
+//            products.push(newProduct);
+
+
+            var html = "<table border='1|1' >";
+            
+            html += "<td>Course Description</td>";
+            html += "<td>Quantity</td>";
+            html += "<td>Duration</td>";
+            html += "<td>Price</td>";
+            html += "<td>Action</td>";
+            for (var i = 0; i < products.length; i++) {
+                html += "<tr>";
+                html += "<td>" + products[i].product_id + "</td>";
+                html += "<td>" + products[i].product_qty+ "</td>";
+                html += "<td>" + products[i].product_dura + " Days" + "</td>";
+                html += "<td>" + "R "  + products[i].product_price + "</td>";
+                html += "<td><button type='submit' onClick='deleteProduct(\"" + products[i].product_id + "\", this);'/>Delete Item</button> &nbsp <button type='submit' onClick='addCart(\"" + products[i].product_id + "\", this);'/>Add to Cart</button></td>";
+                html += "</tr>";
+            }
+            html += "</table>";
+            document.getElementById("demo").innerHTML = html;
+
+        }
+        function deleteProduct(product_id, e) {
+            e.parentNode.parentNode.parentNode.removeChild(e.parentNode.parentNode);
+            for (var i = 0; i < products.length; i++) {
+                if (products[i].product_id == product_id) {
+                    products.splice(i, 1);
+                }
+            }
         }
 
-        //if check returns false,  item was not found in the array.
-        if(!check){ cart.push(item)}
+        function addCart(product_id) {
 
-        //else push to array
-    } else {
-        cart.push(item);
+            for (var i = 0; i < products.length; i++) {
+                if (products[i].product_id == product_id) {
+                    var cartItem = null;
+                    for (var k = 0; k < cart.length; k++) {
+                        if (cart[k].product.product_id == products[i].product_id) {
+                            cartItem = cart[k];
+                            cart[k].product_qty++;
+                            break;
+                        }
+                    }
+                    if (cartItem == null) {
+                        
+                        var cartItem = {
+                            product: products[i],
+                            product_qty: products[i].product_qty 
+                        };
+                        cart.push(cartItem);
+                    }
+                }
+            }
+
+            renderCartTable();
+
+        }
+
+        function renderCartTable() {
+        var html = '';
+        var ele = document.getElementById("demo2");
+        ele.innerHTML = ''; 
+
+        html += "<table id='tblCart' border='1|1'>";
+        html += "<td>Course Description</td>";
+        html += "<td>Quantity</td>";
+        html += "<td>Duration</td>";
+        html += "<td>Price</td>";
+        html += "<td>Total</td>";
+        html += "<td>Action</td></tr>";
+        var GrandTotal = 0;
+        for (var i = 0; i < cart.length; i++) {
+            html += "<tr>";
+            html += "<td>" + cart[i].product.product_id + "</td>";
+//            html += "<td>" + cart[i].product.product_desc + "</td>";
+            html += "<td>" + cart[i].product_qty + "</td>";
+            html += "<td>" + cart[i].product.product_dura + " Days" +"</td>";
+            html += "<td>" + "R "  +cart[i].product.product_price + "</td>";
+            html += "<td>" + "R "  +parseFloat(cart[i].product.product_price) * parseInt(cart[i].product_qty) + "</td>";
+            html += "<td><button type='submit' onClick='subtractQuantity(\"" + cart[i].product.product_id + "\", this);'/>Subtract Quantity</button> &nbsp<button type='submit' onClick='addQuantity(\"" + cart[i].product.product_id + "\", this);'/>Add Quantity</button> &nbsp<button type='submit' onClick='removeItem(\"" + cart[i].product.product_id + "\", this);'/>Remove Item</button></td>";
+            html += "</tr>";
+
+           GrandTotal += parseFloat(cart[i].product.product_price) * parseInt(cart[i].product_qty);            
+
+        }
+        document.getElementById('demo3').innerHTML = "R " + GrandTotal;
+        html += "</table>";
+        ele.innerHTML = html;
     }
-  
-    console.log(cart);
+
+
+        function subtractQuantity(product_id)
+        {
+            
+            for (var i = 0; i < cart.length; i++) {
+                if (cart[i].product.product_id == product_id) {
+                    cart[i].product_qty--;
+                }
+
+                if (cart[i].product_qty == 0) {
+                    cart.splice(i,1);
+                }
+        
+            }
+            renderCartTable();
+        }
+        function addQuantity(product_id)
+        {
+            
+            for (var i = 0; i < cart.length; i++) {
+                if (cart[i].product.product_id == product_id) {
+                    cart[i].product_qty++;
+                }  
+            }
+            renderCartTable();
+        }
+        function removeItem(product_id)
+        {
+            
+            for (var i = 0; i < cart.length; i++) {
+                if (cart[i].product.product_id == product_id) {
+                    cart.splice(i,1);
+                }
+
+            }
+            renderCartTable();
+        }  
+
+//prettyPhoto              
+// Get the modal
+function modall(){
+var modal = document.getElementById('myModal');
+    
+var open = document.getElementById('openCart');
+    
+    open.onclick = function() { 
+    modal.style.display = "block";}
+    
+//var open1 = document.getElementById('openCart1');
+//    
+//    open1.onclick = function() { 
+//    modal.style.display = "block";}
+//    
+//var open2 = document.getElementById('openCart2');
 //
-    //get cart div
-    var div = document.getElementById('cart');
-    //clear cart
-    div.innerHTML = "";
+//open2.onclick = function() { 
+//modal.style.display = "block";}
+    
 
-    //show cart with updated array
-    for(let value in cart){
 
-        // Create a <li> node
-        var node = document.createElement("LI"); 
-        // Create a text node               
-        var textnode = document.createTextNode(cart[value].name);
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-        // Append the text to <li>
-        node.appendChild(textnode);
-
-        // Append <li> to <ul> with id="myList"                             
-        div.appendChild(node);
-    }
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+    modal.style.display = "none";
 }
-
-function removeCart(name){ // Removes one item
-      for (var i in cart) {
-          if (cart[i].name === name) { // "3" === 3 false
-              cart[i].count--; // cart[i].count --
-              if (cart[i].count === 0) {
-                  cart.splice(i, 1);
-              }
-              break;
-          }
-      }
-      console.log(cart);
-};
-	 
-
-
+}modall();
+    
 //STILL TO DO
 //remove an item from cart
 //write a function that calculates the total of the cart
